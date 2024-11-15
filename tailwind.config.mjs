a0a2450d8316e3f9144ecce93a425258adf21908
Henryck Guaramato <guaramatohenryck@gmail.com>
2024-11-15 00:23:33 -0300

    Setting up

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+import typography from '@tailwindcss/typography'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    colors: {
+      'primary-blue': '#41a4c3',
+      'light-blue': '#60b7cf',
+      'dark-blue': '#32859f',
+      'details-gold': '#c7ae6a',
+      'secondary-gray': '#e6e6e6',
+    },
+    extend: {},
+  },
+  plugins: [typography],
+}
